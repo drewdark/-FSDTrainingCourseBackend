74c549b3dd95a86b21844190fe7e5dafb4366898
Andrew <andrewedark@gmail.com>
2021-11-10 09:43:47 +0000

    Tweaks to controller class

--- src/main/java/com/example/capstone/controller/RequestController.java
+++ src/main/java/com/example/capstone/controller/RequestController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class RequestController {
-
+;
     private final RequestService service;
 
     public RequestController(RequestService service) {
